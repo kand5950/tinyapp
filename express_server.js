bdd20a3f0bf7adb7bad901cc8a623637e906e956
Anit <anittankandasamy@gmail.com>
2022-10-04 05:44:20 +0000

    added short-url and long-url key value to database when a post request is received to /urls

--- express_server.js
+++ express_server.js
@@ -2,15 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
 //generate random 6 char string
 function generateRandomString() {
   const value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789';
@@ -25,19 +16,40 @@ function generateRandomString() {
 	
 }
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(req.body); // Log the POST request body to the console
+  res.redirect(`/urls/${shortURL}`); 
+});
+
+
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.get("/u/:id", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+  if (longURL !== `http://${longURL}`) {
+    longURL = `http://${longURL}`;
+  }
+  res.redirect(longURL);
+})
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
