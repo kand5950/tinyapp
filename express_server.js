9ed44b3523800ae0789bc265171f7f834f743cc2
Anit <anittankandasamy@gmail.com>
2022-10-04 03:00:56 +0000

    added route for /urls/:id && /urls in expressserver.js using accompanying template

--- express_server.js
+++ express_server.js
@@ -15,6 +15,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
 app.get("/set", (req, res) => {
   const a = 1; //test to see if accessoble for /fetch *no it's not accessible anywhere other than in function
   res.send(`a = ${a}`);
