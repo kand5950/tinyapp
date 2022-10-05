9adbd3126e144af4ae7d082e398740120d0b6315
Anit <anittankandasamy@gmail.com>
2022-10-05 23:25:15 +0000

    created register template asking for username and password, returns error 404 because no POST route was put in. added route for get to view page

--- express_server.js
+++ express_server.js
@@ -65,6 +65,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect(`/urls`);
 })
 
+app.get("/register", (req, res) => {
+  const templateVars = { username: req.cookies['username']};
+  res.render("urls_register", templateVars);
+})
+
 //shows urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies['username']};
